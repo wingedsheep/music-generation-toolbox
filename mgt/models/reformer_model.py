69c74b774ba2aacfd67a6e1398d9ada13921c252
vincentbons <vincent.bons@qbuzz.nl>
2021-06-11 15:55:55 +0200

    Try to fix reformer model.

--- mgt/models/reformer_model.py
+++ mgt/models/reformer_model.py
@@ -113,7 +113,7 @@ class ReformerModel(object):
         )
 
         # 0 is used for padding and no loss to be calculated on it
-        training_wrapper = AutoregressiveWrapper(model, ignore_index=0, pad_value=0).cuda()
+        training_wrapper = TrainingWrapper(model, ignore_index=0, pad_value=0).cuda()
         return training_wrapper
 
     def create_optimizer(self):
