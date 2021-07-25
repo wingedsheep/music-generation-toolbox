562784c355a30955ac3020caff1633e3b4991075
vincentbons <vincent.bons@qbuzz.nl>
2021-07-25 15:12:09 +0200

    Added Linear attention transformer model.

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(name='music-generation-toolbox',
       install_requires=['pretty_midi>=0.2.9', 'miditoolkit>=0.1.14', 'scipy>=1.6.3',
                         'pylab-sdk>=1.3.2', 'requests>=2.25.1', 'matplotlib>=3.4.2',
                         'reformer-pytorch>=1.4.2', 'x-transformers>=0.12.1', 'torch~=1.8.1'
-                        'numpy~=1.20.3', 'routing_transformer>=1.5.2'],
+                        'numpy~=1.20.3', 'routing_transformer>=1.5.2', 'linear-attention-transformer>=0.18.1'],
       packages=find_namespace_packages(),
       package_data={"": ["*.mid", "*.midi"]})
