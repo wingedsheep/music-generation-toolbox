f86d6182dfce00145cb6a109662b078f3b5c9e61
vincentbons <vincent.bons@qbuzz.nl>
2021-08-01 16:41:06 +0200

    Updated generate function.

--- mgt/models/compound_word_transformer_model.py
+++ mgt/models/compound_word_transformer_model.py
@@ -225,7 +225,7 @@ entmax = entmax_bisect
 
 
 class CompoundWordAutoregressiveWrapper(nn.Module):
-    def __init__(self, net, ignore_index=-100, pad_value=None):
+    def __init__(self, net: CompoundTransformerWrapper, ignore_index=-100, pad_value=None):
         super().__init__()
         if pad_value is None:
             pad_value = [0, 0, 0, 0, 0, 0, 0]
@@ -235,52 +235,34 @@ class CompoundWordAutoregressiveWrapper(nn.Module):
         self.max_seq_len = net.max_seq_len
 
     @torch.no_grad()
-    def generate(self, start_tokens, seq_len, eos_token=None, temperature=1., filter_logits_fn=top_k, filter_thres=0.9,
-                 **kwargs):
-        device = start_tokens.device
-        was_training = self.net.training
-        num_dims = len(start_tokens.shape)
-
-        if num_dims == 1:
-            start_tokens = start_tokens[None, :]
-
-        b, t = start_tokens.shape
-
+    def generate(self, prompt=None, output_length=100):
         self.net.eval()
-        out = start_tokens
-        mask = kwargs.pop('mask', None)
 
-        if mask is None:
-            mask = torch.full_like(out, True, dtype=torch.bool, device=out.device)
+        if prompt is None:
+            prompt = np.array([[2, 0, 0, 0, 0, 0, 0]])  # Bar
 
-        for _ in range(seq_len):
-            x = out[:, -self.max_seq_len:]
-            mask = mask[:, -self.max_seq_len:]
+        print('------ initiate ------')
+        final_res = []
+        h = None
 
-            logits = self.net(x, mask=mask, **kwargs)[:, -1, :]
+        init_t = torch.from_numpy(prompt).long().cuda()
+        for step in range(prompt.shape[0]):
+            input_ = init_t[step, :].unsqueeze(0).unsqueeze(0)
+            final_res.append(prompt[step, :][None, ...])
+            h, y_type = self.net.forward_hidden(input_)
 
-            if filter_logits_fn in {top_k, top_p}:
-                filtered_logits = filter_logits_fn(logits, thres=filter_thres)
-                probs = F.softmax(filtered_logits / temperature, dim=-1)
+        print('------ generate ------')
+        for _ in range(output_length):
+            # sample others
+            next_arr = self.froward_output_sampling(h, y_type)
+            final_res.append(next_arr[None, ...])
 
-            elif filter_logits_fn is entmax:
-                probs = entmax(logits / temperature, alpha=ENTMAX_ALPHA, dim=-1)
+            # forward
+            input_ = torch.from_numpy(next_arr).long().cuda()
+            input_ = input_.unsqueeze(0).unsqueeze(0)
+            h, y_type, memory = self.forward_hidden(input_)
 
-            sample = torch.multinomial(probs, 1)
-
-            out = torch.cat((out, sample), dim=-1)
-            mask = F.pad(mask, (0, 1), value=True)
-
-            if eos_token is not None and (sample == eos_token).all():
-                break
-
-        out = out[:, t:]
-
-        if num_dims == 1:
-            out = out.squeeze(0)
-
-        self.net.train(was_training)
-        return out
+        return final_res
 
     def calculate_loss(self, predicted, target, loss_mask):
         loss = F.cross_entropy(predicted[:, ...].permute(0, 2, 1), target)
