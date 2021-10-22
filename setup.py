66117876c8d14d74814d10f2a7f47e3435f3887a
vincentbons <vincent.bons@qbuzz.nl>
2021-10-22 18:53:08 +0200

    Torch version 1.10.0.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='music-generation-toolbox',
       license='MIT',
       install_requires=['pretty_midi>=0.2.9', 'miditoolkit>=0.1.15', 'scipy>=1.7.1',
                         'pylab-sdk>=1.3.2', 'requests>=2.26.0', 'matplotlib>=3.4.3',
-                        'reformer-pytorch>=1.4.3', 'x-transformers>=0.20.2', 'torch~=1.10.1',
+                        'reformer-pytorch>=1.4.3', 'x-transformers>=0.20.2', 'torch>=1.10.0',
                         'numpy~=1.21.3', 'routing_transformer>=1.6.0'],
       packages=find_namespace_packages(),
       package_data={"": ["*.mid", "*.midi"]})
