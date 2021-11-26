c9052f448e3c22bee819dde9b5250a8b72d3b91b
vincentbons <vincent.bons@qbuzz.nl>
2021-11-26 17:15:58 +0100

    Updated lucidrains transformer library versions.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,11 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
+## 0.3.1 - 2021-11-26
+
+### Updated
+- lucidrains transformer libraries
+
 ## 0.3.0 - 2021-10-22
 
 ### Added
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
 setup(name='music-generation-toolbox',
-      version='0.3.0',
+      version='0.3.1',
       description='Toolbox for generating music',
       author='Vincent Bons',
       url='https://github.com/wingedsheep/music-generation-toolbox',
@@ -9,7 +9,7 @@ setup(name='music-generation-toolbox',
       license='MIT',
       install_requires=['pretty_midi>=0.2.9', 'miditoolkit>=0.1.15', 'scipy>=1.7.1',
                         'pylab-sdk>=1.3.2', 'requests>=2.26.0', 'matplotlib>=3.4.3',
-                        'reformer-pytorch>=1.4.3', 'x-transformers>=0.20.2', 'torch>=1.10.0',
-                        'numpy~=1.21.3', 'routing_transformer>=1.6.0'],
+                        'reformer-pytorch>=1.4.4', 'x-transformers>=0.20.9', 'torch>=1.10.0',
+                        'numpy~=1.21.3', 'routing_transformer>=1.6.1'],
       packages=find_namespace_packages(),
       package_data={"": ["*.mid", "*.midi"]})
