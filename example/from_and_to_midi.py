ad19a266a6c84747b41f7ecc8320b89f843f92c9
vincentbons <vincent.bons@qbuzz.nl>
2021-12-05 17:40:27 +0100

    Added simplified instruments mapping.

--- example/from_and_to_midi.py
+++ example/from_and_to_midi.py
@@ -3,26 +3,21 @@ import os
 
 from mgt.datamanagers.time_shift_data_manager import TimeShiftDataManager
 
+"""
+Example showing how to parse midi files with different encodings.
+Also outputs midi files showing how these encodings sound when converted back.
+"""
+midi_path = '../data/TheWeeknd-BlindingLights.midi'
+midi_path = os.path.join(os.path.dirname(__file__), midi_path)
 
-def run():
-    """
-    Example showing how to parse midi files with different encodings.
-    Also outputs midi files showing how these encodings sound when converted back.
-    """
-    midi_path = '../data/TheWeeknd-BlindingLights.midi'
-    midi_path = os.path.join(os.path.dirname(__file__), midi_path)
+# Parse midi using timeshift method
+timeshift_data_manager = TimeShiftDataManager()
+timeshift_dataset = timeshift_data_manager.prepare_data([midi_path])
+timeshift_midi = timeshift_data_manager.to_midi(timeshift_dataset.data[0])
+timeshift_midi.save("timeshift.midi")
 
-    # Parse midi using timeshift method
-    timeshift_data_manager = TimeShiftDataManager()
-    timeshift_dataset = timeshift_data_manager.prepare_data([midi_path])
-    timeshift_midi = timeshift_data_manager.to_midi(timeshift_dataset.data[0])
-    timeshift_midi.save("timeshift.midi")
-
-    # Parse midi using remi method
-    remi_data_manager = RemiDataManager()
-    remi_dataset = remi_data_manager.prepare_data([midi_path])
-    remi_midi = remi_data_manager.to_midi(remi_dataset.data[0])
-    remi_midi.save("remi.midi")
-
-
-run()
+# Parse midi using remi method
+remi_data_manager = RemiDataManager()
+remi_dataset = remi_data_manager.prepare_data([midi_path])
+remi_midi = remi_data_manager.to_midi(remi_dataset.data[0])
+remi_midi.save("remi.midi")
--- example/test.py
+++
@@ -1,5 +0,0 @@
-from mgt.datamanagers.compound_word_data_manager import CompoundWordDataManager
-
-data_manager = CompoundWordDataManager()
-remi = data_manager.to_remi([[3, 0, 0, 0, 0, 0, 0]])
-print(remi)
