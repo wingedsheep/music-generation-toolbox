f33d8dc38a1cca02e459a55caf885d9d7fa63499
vincentbons <vincent.bons@qbuzz.nl>
2021-12-12 17:43:58 +0100

    test.

--- mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
@@ -80,11 +80,11 @@ class CompoundWordAutoregressiveWrapper(nn.Module):
                                                                                                                       target)
         # Filter padding indices
         type_loss = calculate_loss(proj_type, target[..., 0], type_mask(target))
-        barbeat_loss = calculate_loss(proj_barbeat, target[..., 1], timing_mask(target))
-        tempo_loss = calculate_loss(proj_tempo, target[..., 2], timing_mask(target))
-        instrument_loss = calculate_loss(proj_instrument, target[..., 3], note_mask(target))
-        pitch_loss = calculate_loss(proj_pitch, target[..., 4], note_mask(target))
-        duration_loss = calculate_loss(proj_duration, target[..., 5], note_mask(target))
-        velocity_loss = calculate_loss(proj_velocity, target[..., 6], note_mask(target))
+        barbeat_loss = calculate_loss(proj_barbeat, target[..., 1], type_mask(target))
+        tempo_loss = calculate_loss(proj_tempo, target[..., 2], type_mask(target))
+        instrument_loss = calculate_loss(proj_instrument, target[..., 3], type_mask(target))
+        pitch_loss = calculate_loss(proj_pitch, target[..., 4], type_mask(target))
+        duration_loss = calculate_loss(proj_duration, target[..., 5], type_mask(target))
+        velocity_loss = calculate_loss(proj_velocity, target[..., 6], type_mask(target))
 
         return type_loss, barbeat_loss, tempo_loss, instrument_loss, pitch_loss, duration_loss, velocity_loss
